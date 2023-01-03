26758d5b9ae15370926b62f0430d8b3ac6039c19
blendthink <admin@blendthink.dev>
2023-01-03 18:38:36 +0900

    Remove scripts/dotfiles.zsh

--- install.zsh
+++ install.zsh
@@ -1,4 +1,17 @@
 #!/usr/bin/env zsh
 
-zsh scripts/dotfiles.zsh
+INSTALL_DIR="$HOME/repos/blendthink/dotfiles"
+
+if [ -d "$INSTALL_DIR" ]; then
+  echo "Updating dotfiles..."
+  git -C "$INSTALL_DIR" pull
+  echo "Completed updating dotfiles ✅ "
+else
+  echo "Installing dotfiles..."
+  git clone https://github.com/blendthink/dotfiles.git "$INSTALL_DIR"
+  echo "Completed installing dotfiles ✅ "
+fi
+
+cd "$INSTALL_DIR" || exit
+
 zsh scripts/homebrew.zsh
