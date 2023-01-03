d35ef1f30ef37fa57f9da3cfcf24404b5ae5b06e
blendthink <admin@blendthink.dev>
2023-01-03 19:46:38 +0900

    Fix to not git clone

--- install.zsh
+++ install.zsh
@@ -1,31 +1,6 @@
 #!/usr/bin/env zsh
 
-if xcode-select -p >/dev/null; then
-  # If your CommandLineTools are too outdated,
-  # Update them from Software Update in System Preferences or run:
-  #   softwareupdate --all --install --force
-  # If that doesn't show you any updates, run:
-  #   rm -rf "$(xcode-select -p)"
-  #   xcode-select --install
-  echo "CommandLineTools is already installed ✅ "
-else
-  echo "Installing CommandLineTools..."
-  xcode-select --install
-  echo "Completed installing CommandLineTools ✅ "
-fi
+SCRIPTS_URL="https://raw.githubusercontent.com/blendthink/dotfiles/main/scripts"
 
-INSTALL_DIR="$HOME/repos/blendthink/dotfiles"
-
-if [ -d "$INSTALL_DIR" ]; then
-  echo "Updating dotfiles..."
-  git -C "$INSTALL_DIR" pull
-  echo "Completed updating dotfiles ✅ "
-else
-  echo "Installing dotfiles..."
-  git clone https://github.com/blendthink/dotfiles.git "$INSTALL_DIR"
-  echo "Completed installing dotfiles ✅ "
-fi
-
-cd "$INSTALL_DIR" || exit
-
-zsh scripts/homebrew.zsh
+curl -fsSL "$SCRIPTS_URL/command-line-tools.zsh" | zsh
+curl -fsSL "$SCRIPTS_URL/homebrew.zsh" | zsh
