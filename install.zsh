0842675edc187980fff9a11894596ab044284830
blendthink <admin@blendthink.dev>
2023-01-03 19:23:45 +0900

    Move CommandLineTools step to install.zsh

--- install.zsh
+++ install.zsh
@@ -1,5 +1,19 @@
 #!/usr/bin/env zsh
 
+if xcode-select -p >/dev/null; then
+  # If your CommandLineTools are too outdated,
+  # Update them from Software Update in System Preferences or run:
+  #   softwareupdate --all --install --force
+  # If that doesn't show you any updates, run:
+  #   rm -rf "$(xcode-select -p)"
+  #   xcode-select --install
+  echo "CommandLineTools is already installed ✅ "
+else
+  echo "Installing CommandLineTools..."
+  xcode-select --install
+  echo "Completed installing CommandLineTools ✅ "
+fi
+
 INSTALL_DIR="$HOME/repos/blendthink/dotfiles"
 
 if [ -d "$INSTALL_DIR" ]; then
--- scripts/homebrew.zsh
+++ scripts/homebrew.zsh
@@ -1,19 +1,5 @@
 #!/usr/bin/env zsh
 
-if xcode-select -p >/dev/null; then
-  # If your CommandLineTools are too outdated,
-  # Update them from Software Update in System Preferences or run:
-  #   softwareupdate --all --install --force
-  # If that doesn't show you any updates, run:
-  #   rm -rf "$(xcode-select -p)"
-  #   xcode-select --install
-  echo "CommandLineTools is already installed ✅ "
-else
-  echo "Installing CommandLineTools..."
-  sudo xcode-select --install
-  echo "Completed installing CommandLineTools ✅ "
-fi
-
 if type brew >/dev/null; then
   echo "Homebrew is already installed ✅ "
 else
