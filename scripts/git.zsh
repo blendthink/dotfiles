70377c15688b2c4186ed47dacbaa27936275df46
blendthink <admin@blendthink.dev>
2023-01-05 01:09:10 +0900

    Add gh, gpg, and pinentry-mac installation process

--- scripts/git.zsh
+++ scripts/git.zsh
@@ -8,3 +8,27 @@ else
   source "$HOME/.zprofile"
   echo "Completed installing Git ✅ "
 fi
+
+if type gh >/dev/null; then
+  echo "GitHub CLI is already installed ✅ "
+else
+  echo "Installing GitHub CLI..."
+  brew install gh
+  echo "Completed installing GitHub CLI ✅ "
+fi
+
+if type gpg >/dev/null; then
+  echo "GnuPG is already installed ✅ "
+else
+  echo "Installing GnuPG..."
+  brew install gnupg
+  echo "Completed installing GnuPG ✅ "
+fi
+
+if type pinentry-mac >/dev/null; then
+  echo "pinentry-mac is already installed ✅ "
+else
+  echo "Installing pinentry-mac..."
+  brew install pinentry-mac
+  echo "Completed installing pinentry-mac ✅ "
+fi
