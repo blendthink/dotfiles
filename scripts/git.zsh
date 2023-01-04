c383121530544404238515c190c2caf2b4ed7234
blendthink <admin@blendthink.dev>
2023-01-05 01:32:51 +0900

    Add Git signature and global settings processing

--- scripts/git.zsh
+++ scripts/git.zsh
@@ -32,3 +32,37 @@ else
   brew install pinentry-mac
   echo "Completed installing pinentry-mac ✅ "
 fi
+
+mkdir -p "$HOME/.gnupg" && chmod 700 "$HOME/.gnupg"
+echo "pinentry-program $(which pinentry-mac)" >"$HOME/.gnupg/gpg-agent.conf"
+gpgconf --kill gpg-agent
+
+if [ "$CI" = true ]; then
+  gpg --pinentry-mode loopback --passphrase passwd --quick-gen-key "Tatsuya Okayama <admin@blendthink.dev>" default default 0
+else
+  gpg --quick-gen-key "Tatsuya Okayama <admin@blendthink.dev>" default default 0
+fi
+
+GPG_KEY_ID=$(gpg --list-secret-keys --with-colons | awk -F: '$1 == "sec" {print $5}' | tail -n 1)
+GPG_PUBLIC_KEY=$(gpg --armor --export "$GPG_KEY_ID")
+
+if [ ! "$CI" = true ]; then
+  yes | gh auth login -h github.com -s admin:gpg_key -p https -w
+  gh api \
+    --method POST \
+    -H "Accept: application/vnd.github+json" \
+    /user/gpg_keys \
+    -f name="blendthink's GPG Key" \
+    -f armored_public_key="$GPG_PUBLIC_KEY"
+fi
+
+git config --global init.defaultBranch main
+git config --global user.name blendthink
+git config --global user.email admin@blendthink.dev
+git config --global user.signingkey "$GPG_KEY_ID"
+git config --global commit.gpgsign true
+git config --global gpg.program "$(which gpg)"
+
+GIT_CONFIG_DIR="$HOME/.config/git"
+mkdir -p "$GIT_CONFIG_DIR"
+curl https://raw.githubusercontent.com/github/gitignore/master/Global/macOS.gitignore -o "$GIT_CONFIG_DIR/ignore"
