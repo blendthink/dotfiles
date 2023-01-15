105b979c9f994e07375ef7710e90248c7ba4b9a6
blendthink <admin@blendthink.dev>
2023-01-15 17:59:27 +0900

    Add completions.zsh and suggestions.zsh to install.zsh

--- install.zsh
+++ install.zsh
@@ -5,3 +5,5 @@ SCRIPTS_URL="https://raw.githubusercontent.com/blendthink/dotfiles/main/scripts"
 curl -fsSL "$SCRIPTS_URL/command-line-tools.zsh" | zsh
 curl -fsSL "$SCRIPTS_URL/homebrew.zsh" | zsh
 curl -fsSL "$SCRIPTS_URL/git.zsh" | zsh
+curl -fsSL "$SCRIPTS_URL/completions.zsh" | zsh
+curl -fsSL "$SCRIPTS_URL/suggestions.zsh" | zsh
