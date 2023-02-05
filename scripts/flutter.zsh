3233e1198add31c9a9c22c4c8b0fd415d3695181
blendthink <admin@blendthink.dev>
2023-02-05 22:30:44 +0900

    Refactor xcodes commands

--- scripts/flutter.zsh
+++ scripts/flutter.zsh
@@ -69,9 +69,7 @@ function install_xcodes() {
 
 function install_xcode() {
   echo "Installing Xcode..."
-  latest_version=$(xcodes list | grep -v '[GM|Beta|Candidate]' | grep -o '^[0-9]\+\.[0-9]\+\.*[0-9]*')
-  xcodes install "$latest_version"
-  xcodes select "$latest_version"
+  xcodes install --latest --select
   echo "Completed installing Xcode ✅ "
 }
 
