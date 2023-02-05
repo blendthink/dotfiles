12a31ce757bdb0146b8c14f86c59e744389fbfe1
blendthink <admin@blendthink.dev>
2023-02-05 20:49:24 +0900

    Remove curl options  '-fsSL'

--- install.zsh
+++ install.zsh
@@ -4,13 +4,13 @@ set -e
 
 SCRIPTS_URL="https://raw.githubusercontent.com/blendthink/dotfiles/main/scripts"
 
-curl -fsSL "$SCRIPTS_URL/command-line-tools.zsh" | zsh
-curl -fsSL "$SCRIPTS_URL/homebrew.zsh" | zsh
-curl -fsSL "$SCRIPTS_URL/git.zsh" | zsh
-curl -fsSL "$SCRIPTS_URL/completions.zsh" | zsh
-curl -fsSL "$SCRIPTS_URL/suggestions.zsh" | zsh
-curl -fsSL "$SCRIPTS_URL/java.zsh" | zsh
-curl -fsSL "$SCRIPTS_URL/flutter.zsh" | zsh
-curl -fsSL "$SCRIPTS_URL/rust.zsh" | zsh
-curl -fsSL "$SCRIPTS_URL/node.zsh" | zsh
-curl -fsSL "$SCRIPTS_URL/useful_tools.zsh" | zsh
+curl "$SCRIPTS_URL/command-line-tools.zsh" | zsh
+curl "$SCRIPTS_URL/homebrew.zsh" | zsh
+curl "$SCRIPTS_URL/git.zsh" | zsh
+curl "$SCRIPTS_URL/completions.zsh" | zsh
+curl "$SCRIPTS_URL/suggestions.zsh" | zsh
+curl "$SCRIPTS_URL/java.zsh" | zsh
+curl "$SCRIPTS_URL/flutter.zsh" | zsh
+curl "$SCRIPTS_URL/rust.zsh" | zsh
+curl "$SCRIPTS_URL/node.zsh" | zsh
+curl "$SCRIPTS_URL/useful_tools.zsh" | zsh
