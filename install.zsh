5ed956fa983fea84b7800657ba6b66c8f556e7c3
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-04-19 23:18:48 +0900

    Add defaults.zsh

--- install.zsh
+++ install.zsh
@@ -4,6 +4,7 @@ set -e
 
 SCRIPTS_URL="https://raw.githubusercontent.com/blendthink/dotfiles/main/scripts"
 
+curl "$SCRIPTS_URL/defaults.zsh" | zsh
 curl "$SCRIPTS_URL/command-line-tools.zsh" | zsh
 curl "$SCRIPTS_URL/homebrew.zsh" | zsh
 curl "$SCRIPTS_URL/git.zsh" | zsh
---
+++ scripts/defaults.zsh
@@ -0,0 +1,41 @@
+#!/usr/bin/env zsh
+
+# FYI:
+# - https://macos-defaults.com
+
+set -e
+
+# Battery
+defaults -currentHost write com.apple.controlcenter.plist BatteryShowPercentage -bool true
+
+# Clock
+defaults write com.apple.menuextra.clock.plist "ShowSeconds" -bool true
+
+# DS_Store
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+killall Finder
+
+# Finder
+defaults write com.apple.finder AnimateWindowZoom -bool false
+defaults write com.apple.finder AppleShowAllFiles -bool true
+defaults write com.apple.finder DisableAllAnimations -bool true
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+defaults write com.apple.finder NewWindowTarget -string "PfDe"
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder ShowRecentTags -bool false
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+killall Finder
+
+# Dock
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock launchanim -bool false
+defaults write com.apple.dock mru-spaces -bool false
+defaults write com.apple.dock orientation -string "bottom"
+defaults write com.apple.dock show-recents -bool false
+defaults write com.apple.dock showAppExposeGestureEnabled -bool true
+defaults write com.apple.dock wvous-br-modifier -int 0
+defaults write com.apple.dock wvous-br-corner -int 13
+killall Dock
