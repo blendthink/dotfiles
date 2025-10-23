1f676323d2ac665ba0dc6a7538717a4b3d38fc37
blendthink <32213113+blendthink@users.noreply.github.com>
2025-10-23 19:14:58 +0900

    Update java.zsh
    Remove Java 8

--- scripts/java.zsh
+++ scripts/java.zsh
@@ -10,7 +10,6 @@ if [[ $(uname -m) == 'arm64' ]]; then
 fi
 
 brew install --cask \
-  temurin@8 \
   temurin@11 \
   temurin@17
 
@@ -21,15 +20,12 @@ else
   cat <<'EOF' >>"$HOME/.zshrc"
 
 # java path
-JAVA_8_HOME=$(/usr/libexec/java_home -v 1.8)
 JAVA_11_HOME=$(/usr/libexec/java_home -v 11)
 JAVA_17_HOME=$(/usr/libexec/java_home -v 17)
 
-export JAVA_8_HOME
 export JAVA_11_HOME
 export JAVA_17_HOME
 
-alias java8='export JAVA_HOME=$JAVA_8_HOME ; PATH="${JAVA_HOME}/bin:${PATH}"'
 alias java11='export JAVA_HOME=$JAVA_11_HOME ; PATH="${JAVA_HOME}/bin:${PATH}"'
 alias java17='export JAVA_HOME=$JAVA_17_HOME ; PATH="${JAVA_HOME}/bin:${PATH}"'
 EOF
